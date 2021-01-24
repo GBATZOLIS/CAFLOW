1beb665c103510041fae93afe58391d4e91d31d5
Georgios Batzolis <gbatz97@Georgioss-MBP.lan>
2021-01-24 15:51:41 +0200

    architecture code

---
+++ caflow/models/modules/blocks/AffineCouplingLayer.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 24 13:29:34 2021
+
+@author: gbatz97
+"""
+
+"""
+The main code for the CouplingLayer has been copied from a deep learning tutorial on normalising flows in the University of Amsterdam
+Tutorial: https://uvadlc-notebooks.readthedocs.io/en/latest/tutorial_notebooks/tutorial11/NF_image_modeling.html
+"""
+
+class AffineCouplingLayer(nn.Module):
+    def __init__(self, c_in, dim, network, mask_info):
+        """
+        Coupling layer inside a normalizing flow.
+        Inputs:
+            c_in - Number of input channels
+            dim = Number of dimensions of the input tensor
+            network - A PyTorch nn.Module constituting the deep neural network for mu and sigma.
+                      Output shape should be twice the channel size as the input, because of the masking system
+            mask_info - Dict. It provides all the information for building the mask.
+                        keywords: 'mask_type'. Value Options = ['channel','checkerboard']
+                                  'shape'. Value: shape of the input tensor. This is needed only if mask_type = 'checkerboard'
+                                  'invert'. Value: Boolean. Whether to invert the mask. This can be used instead of permutation layers.
+        """
+
+        super(AffineCouplingLayer).__init__()
+        self.c_in = c_in
+        self.dim = dim
+        self.network = network
+
+        # For stabilization purposes, we apply a tanh activation function on the scaling output.
+        # This prevents sudden large output values for the scaling that can destabilize training.
+        # To still allow scaling factors smaller or larger than -1 and 1 respectively, 
+        # we have a learnable parameter per dimension, called scaling_factor
+        self.scaling_factor = nn.Parameter(torch.zeros(c_in)) #learnable scaling factor for each dimension
+
+        # Register mask as buffer as it is a tensor which is not a parameter,
+        # but should be part of the modules state.
+        mask = self.create_mask(**mask_info)
+        self.register_buffer('mask', mask)
+
+    def create_mask(self, **kwargs):
+        """
+        output: mask (Binary mask (0 or 1) where 0 denotes that the element should be transformed,
+                      while 1 means the latent will be used as input to the NN._
+        """
+        if kwargs.get('mask_type') == 'channel':
+            c_in = self.c_in
+            mask = torch.cat([torch.ones(c_in//2, dtype=torch.float32),
+                            torch.zeros(c_in-c_in//2, dtype=torch.float32)])
+            infered_shape = tuple([1,c_in]+[1 for i in range(self.dim)])
+            mask = mask.view(infered_shape)
+            if kwargs.get('invert')==True:
+                mask = 1 - mask
+            return mask
+        elif kwargs.get('mask_type') == 'checkerboard':
+            shape = kwargs.get('shape')
+            dims = []
+            for dim_size in shape:
+                dims.append[torch.arange(dim_size, dtype=torch.int32)]
+            mesh_dims = torch.meshgrid(*dims)
+            mesh_dims_sum = torch.sum(mesh_dims, dim=0)
+            mask = torch.fmod(mesh_dims_sum, 2)
+            infered_shape = tuple([1,1]+[dim for dim in shape])
+            mask = mask.to(torch.float32).view(infered_shape)
+            if kwargs.get('invert')==True:
+                mask = 1 - mask
+            return mask
+        
+        else:
+            raise NotImplementedError('mask type has not been implemented')
+
+    def forward(self, z, ldj, reverse=False, cond_rv=None):
+        """
+        Inputs:
+            z - Latent input to the flow
+            ldj - The current ldj of the previous flows.
+                  The ldj of this layer will be added to this tensor.
+            reverse - If True, we apply the inverse of the layer.
+            cond_rv (optional) - Allows external input to condition the flow on
+        """
+
+        # Apply network to masked input
+        z_in = z * self.mask
+        if cond_rv is None:
+            nn_out = self.network(z_in)
+        else:
+            nn_out = self.network(z_in, cond_rv)
+        
+        #get the scaling and translation tensors
+        s, t = nn_out.chunk(2, dim=1)
+
+        # Stabilize scaling output
+        infer_shape = tuple([1,-1]+[1 for i in range(self.dim)])
+        s_fac = self.scaling_factor.exp().view(infer_shape)
+        s = torch.tanh(s / s_fac) * s_fac
+
+        # Mask outputs (only transform the second part)
+        s = s * (1 - self.mask)
+        t = t * (1 - self.mask)
+
+        # Affine transformation
+        if not reverse:
+            # Whether we first shift and then scale, or the other way round,
+            # is a design choice, and usually does not have a big impact
+            z = (z + t) * torch.exp(s)
+            ldj += s.sum(dim=[i+1 for i in range(self.dim)]])
+        else:
+            z = (z * torch.exp(-s)) - t
+            ldj -= s.sum(dim=[i+1 for i in range(self.dim)]])
+
+        return z, ldj
--- caflow/models/modules/mri_to_pet/R_flow.py
+++ caflow/models/modules/mri_to_pet/R_flow.py
@@ -8,80 +8,143 @@ Created on Tue Jan 19 22:02:53 2021
 
 
 
-from caflow.models.modules.blocks import AffineCouplingLayer
+
+from caflow.models.modules.blocks.AffineCouplingLayer import AffineCouplingLayer
 import torch.nn as nn
+from iunets.layers import InvertibleDownsampling1D, InvertibleDownsampling2D, InvertibleDownsampling3D, \
+                          InvertibleChannelMixing1D, InvertibleChannelMixing2D, InvertibleChannelMixing3D
+import torch
 
 
 class flow_block(nn.Module):
-    def __init__(self, shape):
+    def __init__(self, channels, dim, depth, network):
         super(flow_block, self).__init__()
-    
-    def forward(self, h_split, h_pass, logdet=0., reverse=False)
-        if reverse:
-            if h_pass is not None:
-                #concatenate h_split and h_pass
-            else:
-                #use only h_split for the reverse flow block
-            
-            #pass the tensor through the reverse flow block
-            #return h_pass, logdet
+        #shape: (channels, X, Y, Z) for 3D, (channels, X, Y) for 2D
+        #we intend to use fully convolutional models which means that we do not need the real shape. We just need the input channels
+        self.channels = channels 
+        self.dim = dim
+        self.depth = depth
+
+        self.layers = nn.ModuleList()
         
+        #choose the correct invertible downsampling and channel mixining layers from the iUNETS library based on the dimension of the tensor
+        self.InvertibleDownsampling = [InvertibleDownsampling1D, InvertibleDownsampling2D, InvertibleDownsampling3D][dim-1]
+        self.InvertibleChannelMixing = [InvertibleChannelMixing1D, InvertibleChannelMixing2D, InvertibleChannelMixing3D][dim-1]
+        
+        self.layers.append(self.InvertibleDownsampling(in_channels = channels, stride=2, method='cayley', init='squeeze', learnable=True))
+        #new shape: 3D -> (8*channels, X/2, Y/2, Z/2)
+        #           2D -> (4*channels, X/2, Y/2)
+        #           1D -> (2*channels, X/2)
+        
+        transformed_channels = 2**dim*channels
+
+        for _ in range(depth):
+            #append activation layer
+            #append permutation layer
+            #append the affine coupling layer
+
+            self.layers.append(self.InvertibleChannelMixing(in_channels = transformed_channels, 
+                                                            method = 'cayley', learnable=True))
+
+            self.layers.append(AffineCouplingLayer(c_in = transformed_channels, 
+                                                   dim=dim, network=network, 
+                                                   mask_info={'mask_type':'channel', 'invert':False}))
+    
+    def forward(self, h, logdet, reverse):
+        if reverse:
+            h_pass, logdet = self.decode(h, logdet)
         else:
-            #pass h_pass through the forward flow
-            #split the end tensor into h_pass and h_split
-            #return h_split, h_pass, logdet
+            h_pass, logdet = self.encode(h, logdet)
+        
+        return h_pass, logdet
+    
+    def encode(self, h, logdet):
+        for layer in self.layers:
+            if isinstance(layer, self.InvertibleDownsampling) or isinstance(layer, self.InvertibleChannelMixing):
+                #The InvertibleDownsampling and InvertibleChannelMixing Layers introduced by Christian et al. yield unit determinant
+                #This is why they do not contribute to the logdet summation.
+                h = layer(h)
+            else:
+                h, logdet = layer(h, logdet, reverse=False)
+        
+        return h, logdet
 
-
+    def decode(self, h, logdet):
+        for layer in self.layers:
+            if isinstance(layer, self.InvertibleDownsampling) or isinstance(layer, self.InvertibleChannelMixing):
+                h = layer.inverse(h) #we are following the implementational change of InvertibleDownsampling and InvertibleChannelMixing
+            else:
+                h, logdet = layer(h, logdet, reverse=True)
+        
+        return h, logdet
 
 
 class R_flow(nn.Module):
-    def __init__(self, shape, scales, opts):
+    def __init__(self, channels, dim, scales, scale_depth, network, opts):
         super(R_flow, self).__init__()
-
+        
+        self.channels = channels
+        self.dim = dim
+        self.scales = scales
+        
         self.scale_blocks = nn.ModuleList()
-
-        for scale in range(1,scales+1):
-            scale_shape = calculate_scale_shape(scale, original_shape)
-            self.scale_blocks.append(flow_block(shape=scale_shape))
+        
+        for scale in range(self.scales):
+            scale_channels = 2 ** (dim * scale) * self.channels
+            self.scale_blocks.append(flow_block(channels = scale_channels, 
+                                                depth = scale_depth, 
+                                                network = network))
 
         # Create prior distribution for final latent space
         self.prior = torch.distributions.normal.Normal(loc=0.0, scale=1.0)
         self.device = opts.device['R_flow']
 
-    def calculate_scale_shape(scale, original_shape):
-        #use this method to calculate the input shape of the flow block
-        pass
-
-    def forward(y=None, z=None, logdet=0., reverse=False):
+    def forward(self, y=None, z=None, logprior=0., logdet=0., reverse=False):
 
         if reverse:
             assert z is not None
-            y_dec, logdet = self.decode(z,logdet=logdet)
+            y_dec, logdet = self.decode(z, logdet=logdet)
             return y_dec, logdet
         else:
             assert y is not None
-            z_enc, logdet = self.encode(y, logdet=logdet)
-            return z_enc, logdet
+            z_enc, logprior, logdet = self.encode(y, logprior=logprior, logdet=logdet)
+            return z_enc, logprior, logdet
     
-    def encode(y, logdet):
+    def encode(self, y, logprior, logdet):
+        #y is the HR image/scan that we want to encode in the latent space
+        #z_enc: list of the encoded latent tensors in ascending scale order
+        #order: from scale 1 (dim=orig_dim/2) --- to --- scale n (dim=orig_dim/2^n)
 
         h_pass = y
         z_enc = []
-        for scale_block in self.scale_blocks:
-            h_split, h_pass, logdet = scale_block(h_split=None, h_pass=h_pass, logdet=logdet, reverse=False)
+        for i in range(self.scales):
+            if i==self.scales-1:
+                h_split, logdet = self.scale_blocks[i](h=h_pass, logdet=logdet, reverse=False)
+            else:
+                h, logdet = self.scale_blocks[i](h=h_pass, logdet=logdet, reverse=False)
+                h_split, h_pass = h.chunk(2, dim=1)
+            
+            logprior+=self.prior.log_prob(h_split).sum()
             z_enc.append(h_split)
 
-        return z_enc, logdet
+        return z_enc, logprior, logdet
 
 
-    def decode(z, logdet):
+    def decode(self, z, logdet):
+        #z is a list of the latent tensors of the different scales.
+        #The tensors of different scales have been put in an ascending order
+        #z = [h_split(1st scale)-size:D/2, ..., h_split(nth scale)-size:D/2^n]
 
         h_pass=None
-        for h_split, scale_block in zip(reversed(z), reversed(self.scale_blocks)):
-            h_pass, logdet = scale_block(h_split=h_split, h_pass=h_pass, logdet=logdet, reverse=True)
-        y_dec = h_pass
+        for i in range(self.scales):
+            h_split = z[self.scales-1-i]
+            if h_pass==None:
+                concat_pass = h_split
+            else:
+                concat_pass = torch.cat([h_split, h_pass], dim=1)
+            h_pass, logdet = self.scale_blocks[i](h=concat_pass, logdet=logdet, reverse=True)
 
-        return y_dec, logdet
+        return h_pass, logdet
 
 
         
