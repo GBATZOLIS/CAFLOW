correct bug in dequantisation (wrong scaling)

caflow/data/template_dataset.py
@@ -17,7 +17,10 @@ import os
 import torchvision.transforms as transforms
 # from data.image_folder import make_dataset
 from PIL import Image
+import torch
 
+def discretize(sample):
+    return (sample * 255).to(torch.float32)
 
 class TemplateDataset(BaseDataset):
     """A template dataset class for you to implement custom datasets."""
@@ -41,7 +44,7 @@ class TemplateDataset(BaseDataset):
         # define the default transform function. You can use <base_dataset.get_transform>; You can also define your custom transform function
         #self.transform = get_transform(opts)
         #transform_list = [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
-        transform_list = [transforms.ToTensor()]
+        transform_list = [transforms.ToTensor(), discretize]
         self.transform = transforms.Compose(transform_list)
 
     def __getitem__(self, index):

caflow/models/modules/blocks/Dequantisation.py
@@ -34,7 +34,7 @@ class Dequantisation(nn.Module):
             z, ldj = self.sigmoid(z, ldj, reverse=False)
             z = z * self.quants
             ldj += np.log(self.quants) * np.prod(z.shape[1:])
-            z = torch.floor(z).clamp(min=0, max=self.quants-1).to(torch.int32)
+            z = torch.floor(z).clamp(min=0, max=self.quants-1)
         return z, ldj
 
     def sigmoid(self, z, ldj, reverse=False):
@@ -53,7 +53,6 @@ class Dequantisation(nn.Module):
 
     def dequant(self, z, ldj):
         # Transform discrete values to continuous volumes
-        z = z.to(torch.float32)
         z = z + torch.rand_like(z)
         z = z / self.quants
         ldj += -1 * np.log(self.quants) * np.prod(z.shape[1:])