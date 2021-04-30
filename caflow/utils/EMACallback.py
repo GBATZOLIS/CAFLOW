1d94ec02a27a4818473e4ef19f9f0309106b06ac
GBATZOLIS <gb511@cam.ac.uk>
2021-04-30 17:10:11 +0300

    fixing ema bugs

--- caflow/utils/EMACallback.py
+++ caflow/utils/EMACallback.py
@@ -2,7 +2,7 @@ from pytorch_lightning.callbacks import Callback
 from caflow.utils.ExponentialMovingAverage import ExponentialMovingAverage
 
 class EMACallback(Callback):
-    def on_fit_start(trainer, pl_module):
+    def on_fit_start(self, trainer, pl_module):
         pl_module.ema = ExponentialMovingAverage(pl_module.parameters(), decay=0.999)
 
     def on_before_zero_grad(self, trainer, pl_module, optimizer):
