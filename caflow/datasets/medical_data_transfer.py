d6472731597d5768bc1b676603afa07e25a974f3
GBATZOLIS <gb511@cam.ac.uk>
2021-06-14 14:56:10 +0300

    medical dataset inspection file

--- caflow/datasets/medical_data_transfer.py
+++ caflow/datasets/medical_data_transfer.py
@@ -80,6 +80,8 @@ def pairs_for_time_threshold(T : int, info: dict):
             for pet_date in pet_dates:
                 for mri_date in mri_dates:
                     delta = pet_date - mri_date
+                    print(mri_date)
+                    print(pet_date)
                     print('Subject %d -> days difference: %d' % (subjectID, abs(delta.days)))
                     if abs(delta.days) <= T:
                         num_pairs+=1
