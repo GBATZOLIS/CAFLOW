176fae30a19a641b0a5815f45dad3e31d11c8948
GBATZOLIS <gb511@cam.ac.uk>
2021-06-28 19:54:28 +0300

    replace squeeze initialisation by haar initialisation

--- caflow/models/modules/blocks/SqueezeLayer.py
+++ caflow/models/modules/blocks/SqueezeLayer.py
@@ -3,7 +3,7 @@ from iunets.layers import InvertibleDownsampling1D, InvertibleDownsampling2D, In
 import torch.nn as nn
 
 class SqueezeLayer(nn.Module):
-    def __init__(self, channels, dim, stride=2, method='cayley', init='squeeze', learnable=True):
+    def __init__(self, channels, dim, stride=2, method='cayley', init='haar', learnable=True):
         super(SqueezeLayer, self).__init__()
         self.i_squeezelayer = [InvertibleDownsampling1D, InvertibleDownsampling2D, InvertibleDownsampling3D][dim-1](channels, stride, method, init, learnable)
     
